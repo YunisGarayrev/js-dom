b0190e897620b34473937b3fc6c6a1a4054636c2
YunisGarayev <79d2mw0@code.edu.az>
2023-05-02 12:43:53 -0700

    js dom -start

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+function openModal() {
+    const modal = document.querySelector(".window");
+    modal.classList.remove("d-none")
+}
+function closeModal(){
+    const modal = document.querySelector(".window");
+    modal.classList.add("d-none");
+}
+function createStudent(){
+    let nameInp = document.querySelector("input[name='ad']");
+    let surnameInp = document.querySelector("input[name='soyad']");
+    let human={
+        name:nameInp.value,
+        surname:surnameInp.value
+    }
+    let array=[]
+    array.push(human)
+    console.log(array);
+    const tbody = document.querySelector("tbody");
+    for (let i = 0; i < array.length; i++) {
+       
+    tbody.innerHTML += `<tr><td>${i}</td><td>${array[i].name}</td><td>${array[i].surname}</td><td><button>Delete</button></td></tr>`;
+    }
+    nameInp.value = '';
+    surnameInp.value = '';
+    closeModal();
+    
+    
+}
+function deleterow(){
+        const row=document.querySelector(".window");
+        row.classList.remove(".row")
+    }
